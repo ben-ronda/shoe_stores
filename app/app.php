bdf14b555e582aee98e120def2c341f0fa05d767
Ben Ronda <bronda95@comcast.net>
2016-03-04 16:16:16 -0800

    add more routes and views

--- app/app.php
+++ app/app.php
@@ -17,4 +17,30 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
      'twig.path' => __DIR__.'/../views'
     ));
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('index.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
+    });
+
+    $app->post("/added_brands", function() use ($app){
+        $new_brand = new Brand($_POST['brand_name']);
+        $new_brand->save();
+        return $app['twig']->render('index.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
+    });
+
+    $app->post("/added_stores", function() use ($app){
+        $new_store = new Store($_POST['store_name']);
+        $new_store->save();
+        return $app['twig']->render('index.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
+    });
+
+    $app->get('/add_brand', function() use ($app){
+        return $app['twig']->render('add_brand.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    $app->get('/add_store', function() use ($app){
+        return $app['twig']->render('add_store.html.twig', array('stores' => Store::getAll()));
+    });
+
+    return $app;
 ?>
